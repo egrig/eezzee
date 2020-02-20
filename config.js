61350c17209a3fdf972897eca04fbd69cf18f1c1
mezisar <31483390+mezisar@users.noreply.github.com>
2020-02-21 00:30:18 +0200

    First BETA

---
+++ config.js
@@ -0,0 +1,19 @@
+var config = {
+  //
+  // //Application Title
+  // title : "YouTube",
+  //
+  // //Error Page Title
+  // errorTitle : "Page not Found (404)",
+  //
+  // //Application URL
+  // url : "http://example.com",
+  //
+  // //Application Description
+  // description : "download YouTube videos, audio",
+  //
+  // //Footer text
+  // footerText : "&copy; 2020"
+};
+
+module.exports = config;
